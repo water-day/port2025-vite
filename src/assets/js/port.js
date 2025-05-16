283f5c5d24ec87fe9ccdce94740bebe8aae46459
water-day <wnjsrud322@naver.com>
2025-05-16 16:03:47 +0900

    🤓연락처영역 완료

--- src/assets/js/port.js
+++ src/assets/js/port.js
@@ -10,8 +10,9 @@ export function port() {
         ease: "none",
         scrollTrigger: {
             trigger: "#port",
-            start: "top 56px",
-            end: "+=3000",
+            // start: "top+=50 top", // ← 스크롤이 좀 더 내려간 후에 시작
+            start: "top top", // ← 전체 요소가 뷰포트 상단에 닿을 때 시작
+    end: () => "+=" + document.querySelector(".port__wrap").offsetWidth, // ← 동적으로 설정
             pin: true,
             scrub: 1,
             markers: false,
